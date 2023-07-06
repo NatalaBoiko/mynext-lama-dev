8cb9c55ad14b6ce9d74959b05950fcb43e701c16
Nataliia <natalabojko@gmail.com>
2023-07-06 18:29:42 +0300

    contacts page

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,8 +1,38 @@
+import Button from "@/components/button/button";
+import Image from "next/image";
 import React from "react";
 import styles from "./page.module.css";
+// import contactImg from "public/contact.png";
 
 const Contacts = () => {
-  return <h1 className={styles.main}>Contacts</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="contacts image"
+            fill={true}
+            // width={500}
+            // height={500}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button href="#" title="Send" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Contacts;
