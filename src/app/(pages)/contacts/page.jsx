b5a8e51132b8881d289edebf39ff143010c64926
Nataliia <natalabojko@gmail.com>
2023-07-08 15:27:12 +0300

    asd

--- src/app/(pages)/contacts/page.jsx
+++ src/app/(pages)/contacts/page.jsx
@@ -7,7 +7,7 @@ import contactImg from "../../../../public/contact.png";
 const Contacts = () => {
   return (
     <>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>Let's Keep in Touch &apos; &lsquo; &#39;</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
@@ -17,17 +17,17 @@ const Contacts = () => {
             className={styles.image}
           />
         </div>
-        <form className={styles.form}>
+        {/* <form className={styles.form}>
           <input type="text" placeholder="name" className={styles.input} />
           <input type="text" placeholder="email" className={styles.input} />
           <textarea
             className={styles.textArea}
             placeholder="message"
-            cols="30"
-            rows="10"
+            cols={30}
+            rows={10}
           ></textarea>
           <Button href="#" title="Send" />
-        </form>
+        </form> */}
       </div>
     </>
   );
