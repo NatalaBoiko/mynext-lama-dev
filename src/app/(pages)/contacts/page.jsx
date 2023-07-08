a2a7a197fa322d3bdf09809453ee61d92a388340
Nataliia <natalabojko@gmail.com>
2023-07-08 15:35:54 +0300

    hello

--- src/app/(pages)/contacts/page.jsx
+++ src/app/(pages)/contacts/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 const Contacts = () => {
   return (
     <>
-      <h1 className={styles.title}>Let's Keep in Touch </h1>
+      <h1>hello!</h1>
     </>
   );
 };
