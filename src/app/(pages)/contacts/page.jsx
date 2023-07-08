454d74aa1b8fac86a7ab0b60651c16707caf7155
Nataliia <natalabojko@gmail.com>
2023-07-08 15:29:19 +0300

    asas

--- src/app/(pages)/contacts/page.jsx
+++ src/app/(pages)/contacts/page.jsx
@@ -8,27 +8,6 @@ const Contacts = () => {
   return (
     <>
       <h1 className={styles.title}>Let's Keep in Touch &apos; &lsquo; &#39;</h1>
-      {/* <div className={styles.content}>
-        <div className={styles.imgContainer}>
-          <Image
-            src={contactImg}
-            alt="contacts image"
-            fill={true}
-            className={styles.image}
-          />
-        </div> */}
-      {/* <form className={styles.form}>
-          <input type="text" placeholder="name" className={styles.input} />
-          <input type="text" placeholder="email" className={styles.input} />
-          <textarea
-            className={styles.textArea}
-            placeholder="message"
-            cols={30}
-            rows={10}
-          ></textarea>
-          <Button href="#" title="Send" />
-        </form> */}
-      {/* </div> */}
     </>
   );
 };
