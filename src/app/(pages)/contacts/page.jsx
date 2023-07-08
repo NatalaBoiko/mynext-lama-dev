bbb909e95b64c629527db36db75af52b0f5390ad
Nataliia <natalabojko@gmail.com>
2023-07-08 15:34:27 +0300

    f

--- src/app/(pages)/contacts/page.jsx
+++ src/app/(pages)/contacts/page.jsx
@@ -1,13 +1,10 @@
-// import Button from "@/components/button/button";
-// import Image from "next/image";
 import React from "react";
 import styles from "./page.module.css";
-// import contactImg from "/public/contact.png";
 
 const Contacts = () => {
   return (
     <>
-      <h1 className={styles.title}>Let's Keep in Touch &apos; &lsquo; &#39;</h1>
+      <h1 className={styles.title}>Let's Keep in Touch </h1>
     </>
   );
 };
