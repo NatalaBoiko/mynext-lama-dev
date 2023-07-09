b1c2b2bbe1a40f3ff2a319e57241b56d126022ad
Nataliia <natalabojko@gmail.com>
2023-07-09 15:48:10 +0300

    blog started

--- src/app/(pages)/blog/[id]/page.jsx
+++ src/app/(pages)/blog/[id]/page.jsx
@@ -1,8 +1,51 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./page.module.css";
 
-const BlogPost = () => {
-  return <h1 className={styles.main}>BlogPost</h1>;
+const BlogPost = ({ testId1 }) => {
+  return (
+    <>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
+            corporis, voluptates aut eveniet animi libero fugit vitae maiores
+            veritatis dolor.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/101667/pexels-photo-101667.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt="hourse"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            s
+            src="https://images.pexels.com/photos/101667/pexels-photo-101667.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="hourse"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+          nostrum eius quia molestiae quos reprehenderit. Voluptates provident
+          ratione distinctio nemo asperiores? Error doloremque iure dolorum
+          soluta, amet repellat harum explicabo quis voluptatem? Repudiandae
+          enim sit debitis culpa id facere quis aliquam dicta tenetur reiciendis
+          harum, nulla maiores omnis recusandae ipsa.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default BlogPost;
--- src/app/(pages)/blog/page.jsx
+++ src/app/(pages)/blog/page.jsx
@@ -1,8 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import styles from "./page.module.css";
 
 const Blog = () => {
-  return <h1 className={styles.main}>Blog</h1>;
+  return (
+    <>
+      <Link href="/blog/testId1" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/1317559/pexels-photo-1317559.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="wow"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            libero modi autem beatae animi impedit soluta hic vel corporis natus
+            illo nostrum incidunt quis iste, ut nam cumque quasi expedita. Ipsam
+            tenetur optio soluta quisquam, autem vel voluptatibus perspiciatis
+            iusto voluptas eos id numquam repellat doloremque! Est tempore
+            accusantium commodi?
+          </p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/1317559/pexels-photo-1317559.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="wow"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            libero modi autem beatae animi impedit soluta hic vel corporis natus
+            illo nostrum incidunt quis iste, ut nam cumque quasi expedita. Ipsam
+            tenetur optio soluta quisquam, autem vel voluptatibus perspiciatis
+            iusto voluptas eos id numquam repellat doloremque! Est tempore
+            accusantium commodi?
+          </p>
+        </div>
+      </Link>{" "}
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/1317559/pexels-photo-1317559.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="wow"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            libero modi autem beatae animi impedit soluta hic vel corporis natus
+            illo nostrum incidunt quis iste, ut nam cumque quasi expedita. Ipsam
+            tenetur optio soluta quisquam, autem vel voluptatibus perspiciatis
+            iusto voluptas eos id numquam repellat doloremque! Est tempore
+            accusantium commodi?
+          </p>
+        </div>
+      </Link>{" "}
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/1317559/pexels-photo-1317559.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="wow"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            libero modi autem beatae animi impedit soluta hic vel corporis natus
+            illo nostrum incidunt quis iste, ut nam cumque quasi expedita. Ipsam
+            tenetur optio soluta quisquam, autem vel voluptatibus perspiciatis
+            iusto voluptas eos id numquam repellat doloremque! Est tempore
+            accusantium commodi?
+          </p>
+        </div>
+      </Link>
+    </>
+  );
 };
 
 export default Blog;
