fc460bc1ee7256ac9a253dc6b53ec8fa7a2c6c93
Nataliia <natalabojko@gmail.com>
2023-08-06 21:31:37 +0300

    fetch data on the server side

--- src/app/(pages)/blog/[id]/page.jsx
+++ src/app/(pages)/blog/[id]/page.jsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 import React from "react";
 import styles from "./page.module.css";
 
-const BlogPost = ({ testId1 }) => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const { userId, title, body } = await getData(params.id);
+
   return (
     <>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>title</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            corporis, voluptates aut eveniet animi libero fugit vitae maiores
-            veritatis dolor.
-          </p>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.desc}>{body}</p>
           <div className={styles.author}>
             <Image
               src="https://images.pexels.com/photos/101667/pexels-photo-101667.jpeg?auto=compress&cs=tinysrgb&w=800"
@@ -21,12 +32,11 @@ const BlogPost = ({ testId1 }) => {
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>username</span>
+            <span className={styles.username}>{userId}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            s
             src="https://images.pexels.com/photos/101667/pexels-photo-101667.jpeg?auto=compress&cs=tinysrgb&w=800"
             alt="hourse"
             fill={true}
@@ -35,14 +45,7 @@ const BlogPost = ({ testId1 }) => {
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
-          nostrum eius quia molestiae quos reprehenderit. Voluptates provident
-          ratione distinctio nemo asperiores? Error doloremque iure dolorum
-          soluta, amet repellat harum explicabo quis voluptatem? Repudiandae
-          enim sit debitis culpa id facere quis aliquam dicta tenetur reiciendis
-          harum, nulla maiores omnis recusandae ipsa.
-        </p>
+        <p className={styles.text}>{body}</p>
       </div>
     </>
   );
