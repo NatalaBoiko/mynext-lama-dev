1ac0f1d6b50765a1f001cfbed5c6f534dafeb8ba
Nataliia <natalabojko@gmail.com>
2023-08-13 11:46:25 +0300

    blog/${process.env.DB_URL}

--- src/app/(pages)/blog/page.jsx
+++ src/app/(pages)/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import styles from "./page.module.css";
 
 async function getData() {
-  const res = await fetch(process.env.DB_URL, {
+  const res = await fetch(`${process.env.DB_URL}`, {
     cache: "no-store",
   });
   // console.log(res);
