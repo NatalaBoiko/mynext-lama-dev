6ff514b2c2e36e05d6fa981c718b32cea335c320
Nataliia <natalabojko@gmail.com>
2023-08-20 22:19:17 +0300

    add & delete items

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -6,19 +6,59 @@ import useSWR from "swr";
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Button from "@/components/button/button";
 
 const Dashboard = () => {
   const session = useSession();
-  // console.log(session);
+  console.log(session);
+  console.log(session?.data?.user);
 
   const router = useRouter();
 
-  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  // const { data, error, isLoading } = useSWR(
-  //   "https://jsonplaceholder.typicode.com/posts",
-  //   fetcher
-  // );
-  // console.log(data);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate, error, isLoading } = useSWR(
+    `api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+  console.log(data);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submited");
+
+    const title = e.target[0].value;
+    const description = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          description,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   if (session.status === "loading") {
     return <p>Loading...</p>;
@@ -29,16 +69,49 @@ const Dashboard = () => {
   if (session.status === "authenticated") {
     return (
       <>
-        <h1 className={styles.main}>Dashboard</h1>
+        <div className={styles.container}>
+          <ul className={styles.posts}>
+            {isLoading
+              ? "loading"
+              : data?.map(({ _id, title, description, img, content }) => (
+                  <li key={_id} className={styles.post}>
+                    <div className={styles.imgContainer}>
+                      <Image
+                        src={img}
+                        alt={title}
+                        fill={true}
+                        className={styles.img}
+                      />
+                    </div>
+                    <h2 className={styles.postTitle}>{title}</h2>
+                    <span
+                      className={styles.delete}
+                      onClick={() => handleDelete(_id)}
+                    >
+                      x
+                    </span>
+                  </li>
+                ))}
+          </ul>
+
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <h1 className={styles.title}>Add new post</h1>
+            <input type="text" placeholder="Title" className={styles.input} />
+            <input type="text" placeholder="Desc" className={styles.input} />
+            <input type="text" placeholder="Image" className={styles.input} />
+
+            <textarea
+              placeholder="Content"
+              className={styles.input}
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className={styles.btn}>Send</button>
+          </form>
+        </div>
       </>
     );
   }
-
-  return (
-    <>
-      <h1 className={styles.main}>Dashboard</h1>
-    </>
-  );
 };
 
 export default Dashboard;
--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,10 +4,26 @@ import Post from "@/models/Post";
 
 export const GET = async (request) => {
   // return new NextResponse("it works!", { status: 200 });
+
+  const url = new URL(request.url);
+
+  const username = url.searchParams.get("username");
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database error", { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  const body = await request.json();
+  const newPost = await new Post(body);
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse("Post has been created", { status: 201 });
   } catch (error) {
     return new NextResponse("Database error", { status: 500 });
   }
